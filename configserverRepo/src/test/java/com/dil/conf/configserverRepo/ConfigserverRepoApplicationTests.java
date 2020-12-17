6f82e90c18323c3f2ba6a2dc0dcbc9507b142a29
dilhan <dilhan@rezadmin>
2020-12-17 10:53:07 +0530

    initial commit

---
+++ configserverRepo/src/test/java/com/dil/conf/configserverRepo/ConfigserverRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dil.conf.configserverRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
